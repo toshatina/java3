5eff9edafc290c4ad4801d8146f5adcc4bb51b27
toshatina <toshatina@gmail.com>
2022-03-01 21:16:33 +0400

    деструктор

--- sandbox/src/main/java/vitalich/newpackage/myfirstprogram.java
+++ sandbox/src/main/java/vitalich/newpackage/myfirstprogram.java
@@ -4,18 +4,10 @@ public class myfirstprogram {
 	public static void main(String[] args) {
 
 
-		point p1 = new point();
-		point p2 = new point();
-		p1.x = 0;
-		p1.y = 0;
-		p2.x = 3;
-		p2.y = 4;
-		System.out.println(Distance(p1,p2));
+		point p1 = new point(0,0);
+		point p2 = new point(4,3);
+		System.out.println(p1.Distance(p2));
 	}
-		public static double Distance (point p1, point p2)
-		{
-			return sqrt((((p1.x) - (p2.x)) * ((p1.x) - (p2.x))) + (((p1.y) - (p2.y)) * ((p1.y) - (p2.y))));
 
-		}
 
 }
--- sandbox/src/main/java/vitalich/newpackage/point.java
+++ sandbox/src/main/java/vitalich/newpackage/point.java
@@ -1,6 +1,19 @@
 package vitalich.newpackage;
 
+import static java.lang.Math.sqrt;
+
 public class point {
     public double x;
     public double y;
+    public point (double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public  double Distance (point  point2)
+    {
+        return sqrt((((this.x) - (point2.x)) * ((this.x) - (point2.x))) + (((this.y) - (point2.y)) * ((this.y) - (point2.y))));
+
+    }
 }
